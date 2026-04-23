878edf87f967304d0a11bd47c351003a8bd15083
Christopher Appiah-Thompson <chrsappiah@gmail.com>
2026-04-23 13:46:47 +1000

    Initial Commit

---
+++ AfricanFashionAppTests/AfricanFashionAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  AfricanFashionAppTests.swift
+//  AfricanFashionAppTests
+//
+//  Created by Christopher Appiah-Thompson  on 23/4/2026.
+//
+
+import Testing
+@testable import AfricanFashionApp
+
+struct AfricanFashionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
